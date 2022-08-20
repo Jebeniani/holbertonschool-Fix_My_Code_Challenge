6e25045f6752917e75cd7ec933ed662a4700ba57
Jebeniani <aymenjebeniani05@gmail.com>
2022-08-20 23:43:15 +0100

    user FIX

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -52,6 +52,7 @@ class User():
             return False
         if self.__password is None:
             return False
+
         return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
